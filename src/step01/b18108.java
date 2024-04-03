3ac5f789606cc50364ac6097b4463fccb1c469e5
CurieLee <dlrbfl98@daum.net>
2024-04-03 21:47:15 +0900

    [입출력과사칙연산] 1998년생인 내가 태국에서는 2541년생?!

---
+++ src/step01/b18108.java
@@ -0,0 +1,12 @@
+package step01;
+
+import java.util.Scanner;
+public class b18108 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int year = scan.nextInt();
+
+        System.out.print(year - 543);
+
+    }
+}
