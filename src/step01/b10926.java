76cf5c4721b0a21c838fdd7fd5656617ab5c5596
CurieLee <dlrbfl98@daum.net>
2024-04-03 21:40:52 +0900

    [입출력과사칙연산] ??!

---
+++ src/step01/b10926.java
@@ -0,0 +1,12 @@
+package step01;
+
+import java.util.Scanner;
+
+public class b10926 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String id = scan.next();
+
+        System.out.print(id + "??!");
+    }
+}
