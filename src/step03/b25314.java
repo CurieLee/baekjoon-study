727383f67ab16e0b24876446e34f9d2b76e102fd
CurieLee <dlrbfl98@daum.net>
2024-04-10 02:44:20 +0900

    [반복문] 코딩은 체육과목 입니다

---
+++ src/step03/b25314.java
@@ -0,0 +1,13 @@
+package step03;
+
+import java.util.Scanner;
+public class b25314 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int N = scan.nextInt();
+        for (int i = 0; i < (N / 4); i++)
+            System.out.print("long ");
+        System.out.print("int");
+    }
+}
