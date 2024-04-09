869b3cb2c04c104d24a1e190f529c7cd335e1b2e
CurieLee <dlrbfl98@daum.net>
2024-04-10 02:43:47 +0900

    [반복문] A+B

---
+++ src/step03/b10952.java
@@ -0,0 +1,18 @@
+package step03;
+
+import java.util.Scanner;
+public class b10952 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        while (true) {
+            int a = scan.nextInt();
+            int b = scan.nextInt();
+
+            if (a == 0 && b == 0)
+                break;
+
+            System.out.println(a + b);
+        }
+    }
+}
