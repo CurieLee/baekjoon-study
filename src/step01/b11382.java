75bb1f3f7371417ac4e144e3f1a3b6b0d5473de0
CurieLee <dlrbfl98@daum.net>
2024-04-09 18:09:39 +0900

    [입출력과사칙연산] 꼬마 정민

---
+++ src/step01/b11382.java
@@ -0,0 +1,14 @@
+package step01;
+
+import java.util.Scanner;
+public class b11382 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        long a = scan.nextLong();
+        long b = scan.nextLong();
+        long c = scan.nextLong();
+
+        System.out.print(a + b + c);
+    }
+}
